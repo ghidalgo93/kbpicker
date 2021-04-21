6c0c695faeb5007ea1becedc5de999f61812f17c
ghidalgo93 <ghidalgo93@gmail.com>
2021-04-21 15:25:51 -0600

    fix up some boilerplate syntax

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+router.get("/cool", (req, res, next) => {
+  res.send("youre so freaking cool!");
 });
 
 module.exports = router;
